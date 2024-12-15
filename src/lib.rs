4762651937cec83ce859cd1ef632c3c3d64628fb
Yota Toyama <raviqqe@gmail.com>
2024-12-15 21:25:32 +0900

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,51 @@
 use std::{
     fs::read,
     mem::take,
-    sync::{RwLock, RwLockReadGuard},
+    sync::{OnceLock, RwLock, RwLockReadGuard},
 };
+
+use notify::{Event, RecommendedWatcher, Watcher};
 
 /// A hot reloaded module.
 pub struct HotModule {
+    path: &'static str,
     current: RwLock<Vec<u8>>,
     next: RwLock<Option<Vec<u8>>>,
+    watcher: OnceLock<RecommendedWatcher>,
 }
 
 impl HotModule {
+    /// Crates a new hot reloaded module.
     pub const fn new(path: &'static str) -> Self {
         Self {
+            path,
             current: RwLock::new(Vec::new()),
             next: RwLock::new(None),
+            watcher: OnceLock::new(),
         }
     }
 
     /// Loads a module.
-    pub fn load(&self) -> RwLockReadGuard<Vec<u8>> {
+    pub fn load(&'static self) -> RwLockReadGuard<'static, Vec<u8>> {
+        self.watcher.get_or_init(|| {
+            let mut lock = self.current.write().expect("write lock");
+            *lock = read(self.path).expect("readable file");
+
+            RecommendedWatcher::new(
+                |result| {
+                    if let Ok(Event { kind, .. }) = result {
+                        if kind.is_modify() {
+                            if let Ok(mut content) = self.next.write() {
+                                *content = Some(read(self.path).unwrap());
+                            }
+                        }
+                    }
+                },
+                Default::default(),
+            )
+            .unwrap()
+        });
+
         if let Ok(mut content) = self.next.try_write() {
             if let Some(content) = take(&mut *content) {
                 if let Ok(mut current) = self.current.try_write() {
@@ -30,7 +56,7 @@ impl HotModule {
             }
         }
 
-        self.current.read().expect("lock")
+        self.current.read().expect("read lock")
     }
 }
 
@@ -40,8 +66,8 @@ mod tests {
 
     #[test]
     fn load_file() {
-        static FOO: HotModule = HotModule::new("lib.rs");
+        static FOO: HotModule = HotModule::new("test/foo.txt");
 
-        assert_eq!(*FOO.load(), &[]);
+        assert_eq!(*FOO.load(), "Hello, world!\n".as_bytes());
     }
 }
