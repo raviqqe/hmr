09cbe1bf5f330b5d75edbcc1900ac889ce95b8f3
Yota Toyama <raviqqe@gmail.com>
2024-12-21 18:08:43 +0900

    Derive `Debug`

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ use std::{
 use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher};
 
 /// A hot reloaded module.
+#[derive(Debug)]
 pub struct Module {
     path: &'static str,
     current: RwLock<Vec<u8>>,
@@ -19,6 +20,7 @@ pub struct Module {
 }
 
 /// A read guard of a module.
+#[derive(Debug)]
 pub struct Guard(RwLockReadGuard<'static, Vec<u8>>);
 
 impl Deref for Guard {
